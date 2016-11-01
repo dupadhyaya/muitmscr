dc700363882d645c9f8f73d6b406c1e64d133657
dupadhyaya <dup1966@gmail.com>
2016-11-01 19:35:47 +0530

    02 Nov

--- anova1.R
+++ anova1.R
@@ -1,3 +1,5 @@
+# Anova http://pages.stat.wisc.edu/~yandell/st571/R/anova.pdf
+
 p1=c(9, 12, 14, 11, 13)
 p2=c(10, 6, 9, 9, 10)
 p3=c(12, 14, 11, 13, 11)
@@ -27,3 +29,22 @@ df
 # Confidence Interval
 alpha = c(0.05, 0.01)
 qf(alpha, df["trt"], df["err"], lower.tail = FALSE)
+#
+anova(fit)["Residuals", "Sum Sq"]
+anova(fit)["Residuals", "Sum Sq"]/qchisq(c(0.025, 0.975), 18,lower.tail = FALSE)
+
+
+
+#---------Anova2 
+ratings= c(4,4,6,8,8,9,8,10,8,10,6,6,6,7,5)
+schools = c('Public','Public','Public','Public','Public','Charter','Charter','Charter','Charter','Charter','Private','Private','Private','Private','Private')
+schools
+ratings
+schools = factor(schools)
+fit2 =lm(ratings ~ schools)
+anova(fit2)
+confint(fit2)
+df2 = data.frame(ratings, schools)
+length(ratings)
+length(schools)
+df2
